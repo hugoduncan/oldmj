347b2461f6e807ec43fccca312caf1de6be0b8ee
Hugo Duncan <hugo@hugoduncan.org>
2020-09-24 09:31:28 -0400

    feat: add no-op invoker, and :undefined special target (#29)

--- src/cli/makejack/impl/invokers.clj
+++ src/cli/makejack/impl/invokers.clj
@@ -1,9 +1,11 @@
 (ns makejack.impl.invokers
   (:require makejack.invoke.babashka
             makejack.invoke.clojure
+            makejack.invoke.no-op
             makejack.invoke.shell))
 
 (def invokers
   {:babashka #'makejack.invoke.babashka/babashka
    :clojure  #'makejack.invoke.clojure/clojure
+   :no-op    #'makejack.invoke.no-op/no-op
    :shell    #'makejack.invoke.shell/shell})
---
+++ src/cli/makejack/invoke/no_op.clj
@@ -0,0 +1,14 @@
+(ns makejack.invoke.no-op)
+
+(defn no-op
+  "When the :message key is specified in the target config, prints the message.
+
+  Otherwise, does nothing.
+
+  Useful for specifying an :undefined target to make missing targets not be an error,
+  or as a no-op specification for a specific target."
+  [_args target-kw config _options]
+  (let [target-config (get-in config [:mj :targets target-kw])
+        message (:message target-config)]
+    (when message
+      (println message))))
