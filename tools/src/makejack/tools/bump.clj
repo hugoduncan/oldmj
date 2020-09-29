04b7ad5f716bf01f0f5a98d0bf006af75bd9225c
Hugo Duncan <hugo@hugoduncan.org>
2020-09-29 08:12:19 -0400

    add bump-test

--- tools/src/makejack/tools/bump.clj
+++ tools/src/makejack/tools/bump.clj
@@ -25,21 +25,22 @@
 
 (defn read-version-map
   [version]
+  {:pre [(string? version)]}
   (let [components (mapv maybe-long (str/split version #"[.-]"))]
     (zipmap
      [:major :minor :incremental :qualifier]
      components)))
 
 (defmulti current-version
-  (fn [version-source _project] (:type version-source)))
+  (fn [version-source _project _options] (:type version-source)))
 
 (defmethod current-version :project-edn
-  [_version-source {:keys [version]}]
+  [_version-source {:keys [version]} _options]
   (read-version-map version))
 
 (defmethod current-version :version-edn
-  [{:keys [path]} {:keys [version]}]
-  (edn/read-string (slurp path)))
+  [{:keys [path]} {:keys [version]} {:keys [dir]}]
+  (edn/read-string (slurp (path/as-file (path/path-for dir path)))))
 
 (defn next-version [version-map [part value]]
   (let [part-kw (keyword part)]
@@ -57,6 +58,7 @@
   [path search-regex old-str new-str]
   (let [path            (path/path path)
         content-str     (slurp (path/as-file path))
+        _               (prn :search-regex search-regex :content-str content-str)
         found-str       (re-find search-regex content-str)
         new-found-str   (str/replace found-str old-str new-str)
         new-content-str (str/replace content-str found-str new-found-str)]
@@ -76,19 +78,21 @@
      (util/format-version-map new-version-map))))
 
 (defmethod update-version-source :version-edn
-  [{:keys [path]} _old-version-map new-version-map _options]
-  (spit path (pr-str new-version-map)))
+  [{:keys [path]} _old-version-map new-version-map {:keys [dir]}]
+  (spit
+   (path/as-file (path/path-for dir path))
+   (pr-str new-version-map)))
 
 (defmulti update-version
-  (fn [filedef _old-version-map _new-version-map]
+  (fn [filedef _old-version-map _new-version-map _options]
     (cond
       (string? filedef)    :file-literal
       (path/path? filedef) :file-literal
       {:search filedef}    :file-search)))
 
 (defmethod update-version :file-literal
-  [filedef old-version-map new-version-map]
-  (let [path            (path/path-for filedef)
+  [filedef old-version-map new-version-map {:keys [dir]}]
+  (let [path            (path/path-for dir filedef)
         content-str     (slurp (path/as-file path))
         new-version-str (str/replace
                          content-str
@@ -97,9 +101,9 @@
     (spit (path/as-file path) new-version-str)))
 
 (defmethod update-version :file-search
-  [{:keys [search path]} old-version-map new-version-map]
+  [{:keys [search path]} old-version-map new-version-map {:keys [dir]}]
   (update-file-with-regex
-   path
+   (path/path-for dir path)
    search
    (util/format-version-map old-version-map)
    (util/format-version-map new-version-map)))
@@ -108,11 +112,11 @@
   "Bump project version"
   [options args {:keys [mj project]}]
   (let [version-source (infer-source options)
-        version-map    (current-version version-source project)
+        version-map    (current-version version-source project options)
         new-version    (next-version version-map args)]
-    (update-version-source version-source new-version options)
+    (update-version-source version-source version-map new-version options)
     (doseq [update (:updates options)]
-      (update-version update new-version))))
+      (update-version update version-map new-version options))))
 
 (def extra-options
   [])
--- tools/test/unit/makejack/tools/bump_test.clj
+++ tools/test/unit/makejack/tools/bump_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
             [clojure.test :refer [deftest is testing]]
+            [makejack.api.core :as makejack]
             [makejack.api.filesystem :as filesystem]
             [makejack.api.path :as path]
             [makejack.tools.bump :as bump]))
@@ -29,7 +30,8 @@
               :incremental 0}
              (bump/current-version
               {:type :project-edn}
-              {:version "0.1.0"}))))
+              {:version "0.1.0"}
+              {}))))
     (testing "cfor a :version-edn type, reads the version map"
       (is (= {:major       0
               :minor       1
@@ -37,6 +39,7 @@
              (bump/current-version
               {:type :version-edn
                :path (.getPath (io/resource "version.edn"))}
+              {}
               {}))))))
 
 (deftest update-version-source-test
@@ -75,7 +78,8 @@
           (bump/update-version
            path
            {:major 0 :minor 1 :incremental 0}
-           {:major 1 :minor 2 :incremental 3})
+           {:major 1 :minor 2 :incremental 3}
+           {})
           (is (= ";; comment\n{:version \"1.2.3\"}"
                  (slurp (path/as-file path))))))
       (testing "for a map, updates the old version with the new version in a search"
@@ -85,7 +89,8 @@
           (bump/update-version
            filedef
            {:major 0 :minor 1 :incremental 0}
-           {:major 1 :minor 2 :incremental 3})
+           {:major 1 :minor 2 :incremental 3}
+           {})
           (is (= "{:abc \"1.2.3\" :def \"0.1.0\"}"
                  (slurp (path/as-file path)))))))))
 
@@ -111,3 +116,55 @@
                    (bump/next-version
                     {:major "x"}
                     ["major"]))))))
+
+(deftest bump-test
+  (testing "bump"
+    (testing "for a :project-edn type, updates the version in the project file"
+      (filesystem/with-temp-dir [dir "bump-test"]
+        (filesystem/copy-files!
+         (.getPath (io/resource "test_bump_project_edn"))
+         dir)
+        (bump/bump
+         {:dir     dir
+          :updates ["README.md"
+                    {:path "deps.edn" :search #"/related\s+\{.*\}"}]}
+         ["minor"]
+         (makejack/load-config {:dir dir}))
+        (let [project-edn-path (path/path dir "project.edn")
+              readme-path      (path/path dir "README.md")
+              deps-edn-path    (path/path dir "deps.edn")]
+          (is (= "0.2.0"
+                 (:version (edn/read-string (slurp (path/as-file project-edn-path))))))
+          (is (= "current \"0.2.0\"\n"
+                 (slurp (path/as-file readme-path))))
+          (let [deps-edn (edn/read-string (slurp (path/as-file deps-edn-path)))]
+            (is (= "0.2.0"
+                   (-> deps-edn :deps (get 'related/related) :mvn/version)))
+            (is (= "0.1.0"
+                   (-> deps-edn :deps (get 'unrelated/unrelated) :mvn/version)))))))
+    (testing "for a :version-edn type, writes the version to the version.edn file"
+      (filesystem/with-temp-dir [dir "bump-test"]
+        (filesystem/copy-files!
+         (.getPath (io/resource "test_bump_version_edn"))
+         dir)
+        (bump/bump
+         {:dir     dir
+          :updates ["README.md"
+                    {:path "deps.edn" :search #"/related\s+\{.*\}"}]}
+         ["minor"]
+         (makejack/load-config {:dir dir}))
+        (let [version-edn-path (path/path dir "version.edn")
+              project-edn-path (path/path dir "project.edn")
+              readme-path      (path/path dir "README.md")
+              deps-edn-path    (path/path dir "deps.edn")]
+          (is (= {:major 1 :minor 3 :incremental 3}
+                 (edn/read-string (slurp (path/as-file version-edn-path)))))
+          (is (re-find #":version\s+#version-string"
+                       (slurp (path/as-file project-edn-path))))
+          (is (= "current \"1.3.3\"\n"
+                 (slurp (path/as-file readme-path))))
+          (let [deps-edn (edn/read-string (slurp (path/as-file deps-edn-path)))]
+            (is (= "1.3.3"
+                   (-> deps-edn :deps (get 'related/related) :mvn/version)))
+            (is (= "1.2.3"
+                   (-> deps-edn :deps (get 'unrelated/unrelated) :mvn/version)))))))))
